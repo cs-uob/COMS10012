81fbaf374a808972f382b73bc9326064e2ca1c3c
bristol-d <->
2022-04-05 16:26:21 +0100

    sql setup allows remote connections

--- code/databases/sample-data.sql
+++ code/databases/sample-data.sql
@@ -1,15 +1,15 @@
 -- to be run as root
 
 -- create the default user 'vagrant' so 'mysql' on its own works
-CREATE USER 'vagrant'@'localhost';
+CREATE USER 'vagrant'@'%';
 -- database for people to play around with
 CREATE DATABASE data;
-GRANT ALL ON data.* to 'vagrant'@'localhost';
+GRANT ALL ON data.* to 'vagrant'@'%';
 -- sample data, these are read-only by default to prevent accidents
 CREATE DATABASE census;
-GRANT SELECT ON census.* to 'vagrant'@'localhost';
+GRANT SELECT ON census.* to 'vagrant'@'%';
 CREATE DATABASE elections;
-GRANT SELECT ON elections.* to 'vagrant'@'localhost';
+GRANT SELECT ON elections.* to 'vagrant'@'%';
 
 FLUSH PRIVILEGES;
 
